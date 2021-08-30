4b91b2fb4f971058b31f030e07d124569eacd5e5
Krzysiek Wilczyński <krzysztof.wilczynski@aplitt.pl>
2021-08-30 20:50:28 +0200

    add components switcher

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,44 @@
 <template>
-	<Slider></Slider>
+	<Slider @switch="switchComponent($event)"></Slider>
 	<component :is="currentComponent"></component>
 </template>
 
 <script lang="ts">
-import {
+import {computed,
 	defineAsyncComponent,
-	ref
-} from 'vue';
+	ref} from 'vue';
 import Slider from './components/Slider.vue';
 import screensavers from './components/screensavers';
+
+type switchValue = 'next' | 'previous';
 
 export default {
 	components: {
 		Slider,
 		'Bubbles': defineAsyncComponent(() =>
-			import('./components/screensavers/Bubbles.vue'))
+			import('./components/screensavers/Bubbles.vue')),
+		'Stripes': defineAsyncComponent(() =>
+			import('./components/screensavers/Stripes.vue')),
 	},
 	setup() {
-		// eslint-disable-next-line no-unused-vars
-		const currentComponent = ref(screensavers[0])
+		const componentIndex = ref(0);
+		const currentComponent = computed(() => screensavers[componentIndex.value])
+
+		function switchComponent(value: switchValue) {
+			console.log(currentComponent)
+			if (componentIndex.value === screensavers.length - 1 && value === 'next') {
+				componentIndex.value = 0;
+			} else if (componentIndex.value === 0 && value === 'previous') {
+				componentIndex.value = screensavers.length - 1;
+			} else if (componentIndex.value < screensavers.length && value === 'next') {
+				componentIndex.value++;
+			} else {
+				componentIndex.value--;
+			}
+		}
+
 		return {
-			screensavers, currentComponent
+			screensavers, currentComponent, componentIndex, switchComponent
 		}
 	}
 }
--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -1,16 +1,17 @@
 <template>
 	<div
-		id="left"
+		class="slider"
 		@click="$emit('switch', 'previous')"
 	>
 		<div class="icon">
 		</div>
 	</div>
 	<div
-		id="right"
+		class="slider"
 		@click="$emit('switch', 'next')"
 	>
-		<div class="icon"></div>
+		<div class="icon">
+		</div>
 	</div>
 </template>
 
@@ -22,5 +23,33 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.slider {
+  position: fixed;
+  height: 100%;
+  top: 0;
+  width: 10%;
 
+  &:first-of-type {
+    left: 0;
+
+    &:hover {
+      background: linear-gradient(-90deg, white, black);
+    }
+  }
+
+  &:last-of-type {
+    right: 0;
+
+    &:hover {
+      background: linear-gradient(90deg, white, black);
+    }
+  }
+
+  .icon {
+    position: absolute;
+    transform: translateY(-50%);
+    top: 50%;
+    margin: 0 auto;
+  }
+}
 </style>
